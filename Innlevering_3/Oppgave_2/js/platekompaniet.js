8239c2d2f2dfccf52222c4b9265dff7395cab7b3
Magnus <kjemag12@nith.no>
2014-05-21 20:46:54 +0200

    add + remove + update cart stuff

--- Innlevering_3/Oppgave_2/js/platekompaniet.js
+++ Innlevering_3/Oppgave_2/js/platekompaniet.js
@@ -16,32 +16,30 @@
 
     var setEventHandlers = function() {
         logo.click(function(event) {
-            console.log("logo got clicked");
+            //console.log("logo got clicked");
         });
         cartLink.click(function () {
             $("#expandable").css("display", "block");
+            console.log("expand cart clicked");
             cartLink.addClass("cartOpen");
         });
-
     }
 
     var getJSON = function() {
         $.getJSON( komplettJSON, function(data){
             $.each(data.movies, function(i, item) {
                 $("section#hovedInnhold").append(
-                    "<article>"
+                    "<article id='movieId"+ i +"'>"
                     + "<img src='../" + item.imageSrc + "'/>"
                     + "<h1>" + item.Title + "</h1>"
                     + "<p>" + item.Description + "</p>"
                     + "<div class='price'>"+ getPrice(item.priceCat)+ "</div>"
-                    + "<img id='movieId"+ i +"'' src='../images/buy.png' alt='buy' class='buy' />"
+                    + "<img src='../images/buy.png' alt='buy' class='buy' />"
                     + "</article>"
                 );
             });
             $(".buy").click(function(){
-                putInCart($(this).closest('article').children('h1'),
-                            this.id,
-                            $(this).closest('article').children('img').attr('src'));
+                putInCart($(this));
 
                 $("#expandable").css("display", "block");
                 $("#expandable nav a:first-child").click(function () {
@@ -52,30 +50,83 @@
         });
     }
 
-    var putInCart = function (title, id, imgUrl) {
-        var title = title.html();
-        var movieId = id.substring(7);
+    /*
+        Check if movie allready is added to cart, in that case set an counter of it to +1
+        If the added movie does noe exist in cart add a new object in cart-array
+        Update the cart
+    */
+    var putInCart = function (element) {
+        var title = element.closest('article').children('h1').html();
+        var id = element.closest('article').attr('id').slice(7);
+        var url = element.closest('article').children('img').attr('src');
 
-        console.log("movie id: " + movieId + " is called '" + title + "'");
-        console.log(imgUrl);
-        //Add clicked movie to cart, and update cartMenu
-        cart.push(movieId);
-        updateCartCount();
-        $("#cartContent").append("<div>"+ "<img src='" +  imgUrl + "'/><h2>"+title+"</h2><p></p><a class='delFromCart'>Slett</a></div>");
-        $(".delFromCart").click(function(){
-            delFromChart(this);
+        //console.log("movie id: " + movieId + " is called '" + title + "'");
+        //console.log(imgUrl);
+
+        var found = false;
+        if(cart.length < 1) {
+            console.log("There was nothing in the cart baby, therefor lets add the the movie " + title);
+            cart.push({"movieId" : id, "title" : title, "img" : url, "count" : 1});
+            console.log(title + " added!\n");
+        } else {
+            console.log("Lets check if "+title+" is allready added in your cart...");
+            for(var i=0; i<cart.length; i++) {
+                if(cart[i].movieId === id) {
+                    console.log("Yes it is, you've allready got " + cart[i].count + "x of " + title + " in you cart. Lets just add an extra.");
+                    cart[i].count++;
+                    console.log(title + " added! Wow, now you've got " + cart[i].count + "x of that badboy in your cart!\n");
+                    found = true;
+                    break;
+                }
+            }
+            if(!found) {
+                console.log("Looks like you don't got this movie in your cart, lets add it!");
+                console.log(title + " added!\n");
+                cart.push({"movieId" : id, "title" : title, "img" : url, "count" : 1});
+            }
+        }
+        updateCart();
+    }
+
+    /*
+        Updates the cart bro!
+        Move along officer..
+    */
+    var updateCart = function() {
+        $("#cartContent").html("");
+        cart.count = 0;
+        $.each(cart, function(i, item){
+            $("#cartContent").append("<div><img src='"+item.img+"' alt='"+item.title+"' />"
+                + "<h2>"+item.title+"</h2><p>"+cart[i].count+"x</p><a class='removeItem'>Slett</a></div>");
+            cart.count += item.count;
+        });
+        $(".cart_icon").html("X (<span>" + cart.count + "</span>)");
+        $(".removeItem").click(function(){
+            console.log("Whats up with that stupid movie, I cant stand it! Lets remove it right now!");
+            removeItem($(this).closest('div'));
         });
     }
 
-    var updateCartCount = function() {
-        return $(".cart_icon").html("X (<span>" + cart.length + "</span>)");
-    }
-
-    var delFromChart = function(element) {
-        console.log("delete something!!");
-        ($(element).parent()).remove();
-        cart.splice(-1,1);
-        updateCartCount();
+    /*
+        Lets remove that movie, I hate it!
+    */
+    var removeItem = function(stupidMovie) {
+        for(var i=0; i<cart.length; i++) {
+            console.log("lets find that stinkin " + stupidMovie.children('img').attr('alt') + " movie..");
+            if(cart[i].title === stupidMovie.children('img').attr('alt')) {
+                console.log("found it!");
+                if(cart[i].count <= 1) {
+                    console.log("luckily, there is only one of its kind in your cart, lets just throw it out!");
+                    $(stupidMovie.parent()).remove();
+                    cart[i].remove();
+                } else {
+                    console.log("you've got like " + cart[i].count + "x of that stupid " + stupidMovie.children('img').attr('alt') + " movie, let me throw away just one.");
+                    cart[i].count--;
+                }
+                break;
+            }
+        }
+        updateCart();
     }
 
     var getPrice = function (cat) {
