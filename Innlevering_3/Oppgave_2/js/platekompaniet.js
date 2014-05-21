0d4112cf4862ff7d052f64e22ff16269bbfe9985
SimenHjelden <hjesim12@nith.no>
2014-05-21 09:34:30 +0200

    Lagt til litt JSON og rammeverk for script

---
+++ Innlevering_3/Oppgave_2/js/platekompaniet.js
@@ -0,0 +1,33 @@
+﻿(function(){
+
+    var init = function () {
+
+    }
+
+    var setObjects = function () {
+
+    }
+
+    var setEventHandlers = function () {
+
+    }
+
+    var getJSON = function () {
+        $.ajax({
+            url: "js/movies.json",
+            dataType: "json",
+            success: function (data) {
+                $.each(data.movies, function (i, item) {
+                    $("#hovedInnhold").append(
+                        "<article><img src='" + item.imageSrc + "'<h1>"+ item.Title + "</h1><p>" + item.Description + "</p></article>"
+                       );
+                });
+            }
+        });
+    }
+
+    var expandCart = function () {
+
+    }
+
+})();
