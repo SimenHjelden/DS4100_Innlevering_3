8acd94b360d2eb266a9b55f5d7ebc0657004f118
SimenHjelden <hjesim12@nith.no>
2014-05-21 10:52:42 +0200

    Prøver å lege til event listener på .buy klasse

--- Innlevering_3/Oppgave_2/js/platekompaniet.js
+++ Innlevering_3/Oppgave_2/js/platekompaniet.js
@@ -1,17 +1,18 @@
 ﻿(function(){
 
+    var buyBtn;
+
     var init = function () {
         getJSON();
         //console.log("init funksjon ferdig");
+        setEventHandlers();
     }
 
     var setObjects = function () {
-
+       
     }
 
-    var setEventHandlers = function () {
-
-    }
+  
 
     var getJSON = function () {
         $.ajax({
@@ -20,16 +21,36 @@
             success: function (data) {
                 $.each(data.movies, function (i, item) {
                     $("section#hovedInnhold").append(
-                        "<article><img src='../" + item.imageSrc + "'/>" + "<h1>" + item.Title + "</h1>" + "<p>" + item.Description + "</p>" + "<div class='pris'></div>" + "<img src='../images/buy.png' alt='buy.png' class='buy' />" + "</article>"
+                        "<article><img src='../" + item.imageSrc + "'/>" + "<h1>" + item.Title + "</h1>" + "<p>" + item.Description + "</p>" + "<div class='price'>" + getPrice(item.priceCat) + "</div>" + "<img src='../images/buy.png' alt='buy.png' class='buy' />" + "</article>"
                        );
                 });
             }
         });
+
+        buyBtn = $("img.buy");     
+    }
+
+    var setEventHandlers = function () {
+        buyBtn.click(function () {
+            console.log("Knappen er klikket");
+        });
+    }
+
+    var getPrice = function (cat) {
+        switch (cat) {
+            case "A":
+                return "149.<span class='cents'>50</span>"
+                break;
+            case "B":
+                return "129.<span class='cents'>50</span>"
+                break;
+        }
     }
 
     var expandCart = function () {
 
     }
+
 
     $( document ).ready(init());
 
