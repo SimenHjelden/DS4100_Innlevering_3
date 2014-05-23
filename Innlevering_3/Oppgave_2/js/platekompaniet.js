7ed480d478188b1e73ecbf502ccfda6e2f1ad42e
Magnus <kjemag12@nith.no>
2014-05-23 12:32:09 +0200

    oppg2 price fix

--- Innlevering_3/Oppgave_2/js/platekompaniet.js
+++ Innlevering_3/Oppgave_2/js/platekompaniet.js
@@ -1,6 +1,6 @@
 ﻿(function(){
 
-    var komplettJSON, logo, cart = [], cartLink, cartOpen, cartCloseLink;
+    var komplettJSON, logo, cart = [], cartLink, cartOpen, cartCloseLink, expandable;
 
     var init = function() {
         setObjects();
@@ -10,52 +10,53 @@
 
     var setObjects = function() {
        komplettJSON = "../js/movies.json";
-       logo = $("#logo");
        cartLink = $(".cart_icon");
        cartOpen = false;
        cartCloseLink = $(".hideCart");
+       expandable = $("#expandable");
     }
 
     var setEventHandlers = function () {
         cartLink.click(function () {
             if (!cartOpen) {
-                $("#expandable").css("display", "block");
+                expandable.css("display", "block");
                 console.log("expand cart clicked");
                 cartLink.addClass("cartOpen");
-                cartOpen = true;
             }
             else {
-                $("#expandable").css("display", "none");
-                cartOpen = false;
+                expandable.css("display", "none");
                 cartLink.removeClass("cartOpen");
             }
+            cartOpen = !(cartOpen);
         });
       
         cartCloseLink.click(function () {
-            $("#expandable").css("display", "none");
+            expandable.css("display", "none");
             cartLink.removeClass("cartOpen");
-            cartOpen = false;
+            cartOpen = !(cartOpen);
          });
     }
 
+    /*
+        Reads the json-object containing all avalible movies, 
+        and renders them as articles in the section with id of hovedInnhold
+    */
     var getJSON = function() {
         $.getJSON( komplettJSON, function(data){
             $.each(data.movies, function(i, item) {
-                var price = getPrice(item.priceCat);
                 $("section#hovedInnhold").append(
                     "<article id='movieId"+ i +"'>"
                     + "<img src='../" + item.imageSrc + "'/>"
                     + "<h1>" + item.Title + "</h1>"
                     + "<p>" + item.Description + "</p>"
-                    + "<div class='price'>" + price.slice(0,-2) + "<span class='cents'>" + price.substr(price.length - 2) + "</span></div>"
+                    + "<div class='price'>" + getPrice(item.priceCat, 1) + "</span></div>"
                     + "<img src='../images/buy.png' alt='buy' class='buy' />"
                     + "</article>"
                 );
             });
             $(".buy").click(function(){
                 putInCart($(this));
-
-                $("#expandable").css("display", "block");
+                expandable.css("display", "block");
                 cartOpen = true;
             });
         });
@@ -70,11 +71,8 @@
         var title = element.closest('article').children('h1').html();
         var id = element.closest('article').attr('id').slice(7);
         var url = element.closest('article').children('img').attr('src');
-
-        //console.log("movie id: " + movieId + " is called '" + title + "'");
-        //console.log(imgUrl);
-
         var found = false;
+
         if(cart.length < 1) {
             console.log("There was nothing in the cart baby, therefor lets add the the movie " + title);
             addMovie(id, title, url, 1);
@@ -99,13 +97,14 @@
         }
     }
 
+    /*
+        Adds a move to the users cart
+    */
     var addMovie = function(id, title, url, count) {
         $.getJSON( komplettJSON, function(data){
-            var price = getPrice(data.movies[id].priceCat);
-            cart.push({"movieId" : id, "title" : title, "img" : url, "count" : 1, "price" : price});
+            cart.push({"movieId" : id, "title" : title, "img" : url, "count" : 1, "price" : data.movies[id].priceCat});
             updateCart();
         });
-        
     }
 
     /*
@@ -120,7 +119,7 @@
                 "<div>"
                     +"<img src='"+item.img+"' alt='"+item.title+"' />"
                     + "<h2>"+item.title+"</h2></br><p class='movieCount'>"+cart[i].count+"x</p></br>"
-                    +"<p class='price'>Pris: "+ (parseFloat(cart[i].count) * parseFloat(cart[i].price)).toFixed(2) +"NOK</p>"
+                    +"<p class='price'>Pris: "+ getPrice(cart[i].price, cart[i].count) + "NOK</p>"
                     +"<a class='removeItem'></a>"
                 +"</div>");
             cart.count += item.count;
@@ -131,7 +130,7 @@
             removeItem($(this).closest('div'));
         });
         if(cart.count == "0") {
-            $("#expandable").hide();
+            expandable.hide();
         }
     }
 
@@ -144,37 +143,35 @@
             if(cart[i].title === stupidMovie.children('img').attr('alt')) {
                 console.log("found it!");
                 if(cart[i].count <= 1) {
-                    console.log("luckily, there is only one of its kind in your cart, lets just throw it out!");
-                    removeFromCart(cart[i]);
-                    updateCart();
+                    console.log("luckily, there is only one of its kind in your cart, lets just throw it out!\n");
+                    cart = jQuery.grep(cart, function(value){ return value != cart[i]; });
                 } else {
                     console.log("you've got like " + cart[i].count + "x of that stupid " + stupidMovie.children('img').attr('alt') + " movie, let me throw away just one.\n");
                     cart[i].count--;
                 }
+                updateCart();
                 break;
             }
         }
-        updateCart();
     }
 
-    var removeFromCart = function(item) {
-        cart = jQuery.grep(cart, function(value){
-            return value != item;
-        });
-    }
-
-    var getPrice = function (cat) {
+    /*
+        Lets find the price based on price category and count
+        This function returns it as e.g. 240.<span class='cents'>50</span>
+    */
+    var getPrice = function(cat, count) {
+        var pris;
         switch (cat) {
             case "A":
-                return "149.50"
+                pris = "149.50";
                 break;
             case "B":
-                return "129.50"
+                pris = "129.50";
                 break;
         }
+        pris = (parseFloat(count) * parseFloat(pris)).toFixed(2)
+        return pris.slice(0,-2) + "<span class='cents'>" + pris.substr(pris.length - 2) + "</span>";
     }
-
-
 
     $( document ).ready(init());
 
