211fc94740433a48e3fcf9481399b435b3867286
SimenHjelden <hjesim12@nith.no>
2014-05-26 09:50:59 +0200

    Lagt til inviduell oppgave

---
+++ Individuell-Simen/js/script.js
@@ -0,0 +1,65 @@
+﻿(function () {
+
+    var init = function () {
+        displaySuperheroes();
+        setEventHandlers();
+        dropableRegions();
+    }
+
+    var displaySuperheroes = function () {
+        $.ajax({
+            url: "js/superhelter.json",
+            dataType: "json",
+            success: function (data) {
+                $.each(data.superhelter, function (i, item) {
+                    $("#galleri").append('<img src="' + item.bildeSrc + '" class="dragImg"/>');
+                    $("#galleri img").draggable({ revert: true });
+                });
+            }
+        });
+    };
+
+    var dropableRegions = function () {
+
+        $("#dropZone").droppable({
+            accept: ".dragImg",
+            cursor: "move",
+            drop: function (event, ui) {
+                $(ui.draggable).detach().css({ top: 0, left: 0 }).appendTo($("#dropZone"));
+                ui.draggable.draggable("disable");
+                $.ajax({
+                    url: "js/superhelter.json",
+                    dataType: "json",
+                    success: function (data) {
+                        $.each(data.superhelter, function (i, item) {
+                            if (item.bildeSrc === $(ui.draggable).attr("src")) {
+                                $("table").append("<tr>"
+                                                    + "<td>" + item.navn + "</td>"
+                                                    + "<td>" + item.realName + "</td>"
+                                                    + "<td>" + item.power + "</td>"
+                                                    + "<td>" + item.weakness + "</td>"
+                                                    + "<td>" + item.info + "</td>"
+                                                    + "</tr>");
+                                $("#statusBar").progressbar({
+                                    value: 50
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    var setEventHandlers = function () {
+        $("#leggTilAlle").click(function(){
+            $("#dropZone .dragImg").detach().css({ top: 0, left: 0 }).appendTo($("#galleri"));
+            $("table tr").not("tr:first-child").remove();
+            $("#galleri .dragImg").draggable("enable");
+        });
+    }
+
+
+    window.onload = init;
+
+})();
