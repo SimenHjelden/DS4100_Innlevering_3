f8d0f10a6de7c2d31fd72c1c78db9b8225d6a069
SimenHjelden <hjesim12@nith.no>
2014-05-29 15:47:49 +0200

    Jobbet litt med økonomilappen

---
+++ Innlevering_3/Okonomilappen/js/quiz.js
@@ -0,0 +1,43 @@
+﻿(function () {
+
+    var questionContainer, alternativesContainer, btnSubmit, currentQuestion;
+
+    var init = function () {
+        setObjects();
+        getJSON();
+    }
+
+    var setObjects = function () {
+        questionContainer = $("#questionContainer");
+        alternativesContainer = $("#alternativesContainer");
+        btnSubmit = $("#btnSubmit");
+        currentQuestion = 0;
+    }
+
+    var setEventHandlers = function () {
+        btnSubmit.click(function () {
+
+
+        });
+    }
+
+    var getJSON = function(){
+        $.ajax({
+            url: "js/Oppgaver.json",
+            dataType: "json",
+            success: function (data) {
+                $.each(data.Oppgaver, function(i, item) {
+                    if (i == currentQuestion) {
+                        questionContainer.append("<h2>Spørsmål: " + i + item.Spm + "</h2>");
+                        $.each(item.Alternativ, function (i, alternativ) {
+                            alternativesContainer.append("<input type='radio' class='radioAlternativ' value='" + alternativ.Text + "'/>");
+                        });
+                    }
+                });
+            }
+        });
+    }
+
+    window.onload = init;
+
+})();
